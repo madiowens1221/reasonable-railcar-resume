c634f575c9d5a03177fd57a06289f11f2237770c
Jeremy Taghizadeh <Jtaghiza@gmail.com>
2018-02-09 16:21:29 -0600

    Add Routing and Home page
    This will redirect the user to /home upon visiting the page. The app
    component will be responsible for the header and the footer.

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -1,14 +1,21 @@
 import { BrowserModule } from '@angular/platform-browser';
 import { NgModule } from '@angular/core';
+import {RouterModule, Routes} from '@angular/router';
 
 import { AppComponent } from './app.component';
+import { AppRoutingModule } from 'app/app-routing/app-routing.module';
+import { HomeComponent } from './home/home.component';
 
 @NgModule({
   declarations: [
-    AppComponent
+    AppComponent,
+    HomeComponent
   ],
   imports: [
-    BrowserModule
+    //Vendor Modules
+    BrowserModule,
+    //Custom Modules
+    AppRoutingModule
   ],
   providers: [],
   bootstrap: [AppComponent]
