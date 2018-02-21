dfc5ef7ba76198b7cee0287be02f69bbe863b1f5
Jeremy Taghizadeh <Jtaghiza@gmail.com>
2018-02-21 00:08:14 -0600

    Add revolve animation to test page
    Added revolving button to test page. This will soon take the place of
    the navigation of the website.
    Code for the animation was found here:
    http://www.useragentman.com/blog/2013/03/03/animating-circular-paths-using-css3-transitions/#

--- src/app/testpage/testpage.component.ts
+++ src/app/testpage/testpage.component.ts
@@ -1,10 +1,28 @@
 import { Component, OnInit } from '@angular/core';
 import { TdFadeInOutAnimation } from '@covalent/core/common';
+import {
+  trigger,
+  state,
+  style,
+  animate,
+  transition,
+  keyframes
+} from '@angular/animations';
 
 @Component({
   selector: 'app-testpage',
   templateUrl: './testpage.component.html',
   styleUrls: ['./testpage.component.css'],
+  animations: [
+    trigger('flyInOut', [
+      state('false', style({transform: 'translateX(150px)'})),
+      state('true', style({transform: 'translateX(150px)'})),
+      transition('true <=> false', animate('5000ms', keyframes([
+        style({opacity: 1, transform: 'rotate(0deg) translateX(150px) rotate(0deg)', offset: 0}),
+        style({opacity: 1, transform: 'rotate(360deg) translateX(150px) rotate(-360deg)', offset: 1})
+      ]))),
+    ])
+  ]
 })
 
 export class TestpageComponent implements OnInit {
@@ -14,5 +32,7 @@ export class TestpageComponent implements OnInit {
   ngOnInit() {
     this.triggerState = true;
   }
- 
+  print() {
+    console.log('i work');    
+  }
 }
