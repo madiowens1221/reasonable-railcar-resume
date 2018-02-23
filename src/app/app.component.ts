58cd6508a06b2043e31f31a96914e469ba97eac0
Jeremy Taghizadeh <Jtaghiza@gmail.com>
2018-02-23 16:11:35 -0600

    Add assets and beautify environment
    Added assets to display space and placed them relativly where I want
    them. This can all change later.

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,17 +1,16 @@
 import { Component, ViewChild, ElementRef } from '@angular/core';
 import { MatButtonModule } from '@angular/material/button';
-import { Router, NavigationEnd } from '@angular/router';
 import { TdFadeInOutAnimation } from '@covalent/core/common';
 
 @Component({
   selector: 'app-root',
   templateUrl: './app.component.html',
-  styleUrls: ['./app.component.css'],
+  styleUrls: ['./app.component.scss'],
 })
 export class AppComponent {
   
 
-  constructor(private _router: Router) {
+  constructor() {
     
   }
 
--- src/app/edu/edu.component.ts
+++ src/app/edu/edu.component.ts
@@ -3,7 +3,7 @@ import { Component, OnInit } from '@angular/core';
 @Component({
   selector: 'app-edu',
   templateUrl: './edu.component.html',
-  styleUrls: ['./edu.component.css']
+  styleUrls: ['./edu.component.scss']
 })
 export class EduComponent implements OnInit {
 
