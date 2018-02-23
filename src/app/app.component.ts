8efdfbf11c074ab0d974ed0f502a213891e26613
Jeremy Taghizadeh <Jtaghiza@gmail.com>
2018-02-23 14:40:22 -0600

    Remove header, add orbiting buttons
    Removed the header from the project and replaced it with orbiting
    buttons on the home page. This will be the basis for navigation in the
    project.

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -2,68 +2,18 @@ import { Component, ViewChild, ElementRef } from '@angular/core';
 import { MatButtonModule } from '@angular/material/button';
 import { Router, NavigationEnd } from '@angular/router';
 import { TdFadeInOutAnimation } from '@covalent/core/common';
-import {routerTransition} from './router-transition/router.animations';
 
 @Component({
   selector: 'app-root',
   templateUrl: './app.component.html',
   styleUrls: ['./app.component.css'],
-  animations: [
-    routerTransition
-  ]
 })
 export class AppComponent {
-  router: Router;
-
-  isHomeRaised: boolean = true;
-  isEduRaised: boolean = false;
-  isSkillsRaised: boolean = false;
-  isHiProjRaised: boolean = false;
-  isWrkExpRaised: boolean = false;
+  
 
   constructor(private _router: Router) {
-    this.router = _router;
-    this.router.events.subscribe(event => {
-      if(event instanceof NavigationEnd) {
-        this.checkRoute();
-      }
-    });
+    
   }
 
-  getState(outlet):boolean {
-    return outlet.activatedRouteData.state;
-  }
 
-  checkRoute() {
-    if(this.router.url === "/home")
-    {
-      this.isHomeRaised = true;
-
-      this.isEduRaised = this.isHiProjRaised = this.isSkillsRaised = this.isWrkExpRaised = false;
-    }
-    if(this.router.url === "/education")
-    {
-      this.isEduRaised = true;
-      this.isHiProjRaised = this.isHomeRaised = this.isSkillsRaised = this.isWrkExpRaised = false;
-      
-    }
-    if(this.router.url === "/skills")
-    {
-      this.isSkillsRaised = true;
-      this.isEduRaised = this.isHiProjRaised = this.isHomeRaised = this.isWrkExpRaised = false;
-      
-    }
-    if(this.router.url === "/highlighted-projects")
-    {
-      this.isHiProjRaised = true;
-      this.isEduRaised = this.isHomeRaised = this.isSkillsRaised = this.isWrkExpRaised = false;
-      
-    }
-    if(this.router.url === "/work-experience")
-    {
-      this.isWrkExpRaised = true;
-      this.isEduRaised = this.isHiProjRaised = this.isHomeRaised = this.isSkillsRaised = false;
-      
-    }
-  }
 }
--- src/app/testpage/testpage.component.ts
+++ src/app/testpage/testpage.component.ts
@@ -13,26 +13,14 @@ import {
   selector: 'app-testpage',
   templateUrl: './testpage.component.html',
   styleUrls: ['./testpage.component.css'],
-  animations: [
-    trigger('flyInOut', [
-      state('false', style({transform: 'translateX(150px)'})),
-      state('true', style({transform: 'translateX(150px)'})),
-      transition('true <=> false', animate('5000ms', keyframes([
-        style({opacity: 1, transform: 'rotate(0deg) translateX(150px) rotate(0deg)', offset: 0}),
-        style({opacity: 1, transform: 'rotate(360deg) translateX(150px) rotate(-360deg)', offset: 1})
-      ]))),
-    ])
-  ]
+  
 })
 
 export class TestpageComponent implements OnInit {
-  triggerState: boolean = false;
+  
   constructor() { }
 
   ngOnInit() {
-    this.triggerState = true;
   }
-  print() {
-    console.log('i work');    
-  }
+ 
 }
